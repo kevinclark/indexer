0577939a0eedbf720283e996600b7d592662f2e2
Kevin Clark <kevinc@greplin.com>
2013-07-12 14:56:24 +0530

    Add support for changing doc id type (now uint32)

--- indexing/index.go
+++ indexing/index.go
@@ -13,18 +13,20 @@ const (
   magic = "searchme"
 )
 
+type DocId uint32
+
 type Index struct {
-	DocCounter  uint64
-	docIdToPath map[uint64]string
-	pathToDocId map[string]uint64
-	postings    map[string][]uint64
+	DocCounter  DocId
+	docIdToPath map[DocId]string
+	pathToDocId map[string]DocId
+	postings    map[string][]DocId
 }
 
 func NewIndex() *Index {
 	var index Index
-	index.postings = make(map[string][]uint64)
-	index.docIdToPath = make(map[uint64]string)
-	index.pathToDocId = make(map[string]uint64)
+	index.postings = make(map[string][]DocId)
+	index.docIdToPath = make(map[DocId]string)
+	index.pathToDocId = make(map[string]DocId)
 	return &index
 }
 
@@ -34,7 +36,7 @@ func (i *Index) Add(doc *Document) error {
 		termSet[term] = true
 	}
 
-	docId := i.DocId(doc.Path)
+	docId := i.AssignDocId(doc.Path)
 
 	for term, _ := range termSet {
 		i.postings[term] = append(i.postings[term], docId)
@@ -43,7 +45,7 @@ func (i *Index) Add(doc *Document) error {
 	return nil
 }
 
-func (i *Index) DocId(path string) uint64 {
+func (i *Index) AssignDocId(path string) DocId {
 	docId, contains := i.pathToDocId[path]
 	if !contains {
 		docId = i.DocCounter
@@ -69,7 +71,7 @@ func (index *Index) TermPaths(term string) []string {
 func (index *Index) Write(w io.Writer) {
   // List of files, null terminated. Doc ids correspond to index. Ends with an empty filename. 
   io.WriteString(w, magic + "\x00")
-  for i := uint64(0); i < index.DocCounter; i++ {
+  for i := DocId(0); i < index.DocCounter; i++ {
     io.WriteString(w, index.docIdToPath[i] + "\x00")
   }
   io.WriteString(w, "\x00")
@@ -99,7 +101,7 @@ func LoadIndex(reader io.Reader) (*Index, error) {
   }
 
   // Read docs
-  var i uint64
+  var i DocId
   for p, _ := r.ReadBytes('\x00'); len(p) >= 2; p, _ = r.ReadBytes('\x00') {
     path := stripNull(p)
     index.docIdToPath[i] = path // skip \x00
@@ -116,9 +118,9 @@ func LoadIndex(reader io.Reader) (*Index, error) {
     term := stripNull(t)
     var size uint32
     binary.Read(r, binary.BigEndian, &size)
-    docs := make([]uint64, size)
+    docs := make([]DocId, size)
     for j := uint32(0); j < size; j++ {
-      var docId uint64
+      var docId DocId
       binary.Read(r, binary.BigEndian, &docId)
       docs[j] = docId
     }
